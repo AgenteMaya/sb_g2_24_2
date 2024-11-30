68598616d9de96d04c3c2bcb8a23629764b84b13
maduda <mariaeduardalivros@gmail.com>
2024-11-30 12:06:49 -0300

    teste3 FFP passou tbm

--- teste3IP.c
+++ teste3IP.c
@@ -54,7 +54,8 @@ int main(void)
     } */
 
     //FIX - FIX - IND
-    DescParam params_F_F_I[3];
+    
+    /* DescParam params_F_F_I[3];
     unsigned char codigo_F_F_I[500];
     func_ptr_0_P teste_F_F_I;
 
@@ -84,9 +85,38 @@ int main(void)
     else{
         printf("NÃO PASSOU NO TESTE\n");
 
-    } 
-        //FIX - FIX - IND
+    }  */
+    
+    //FIX - FIX - PARAM
 
+    DescParam params_F_F_I[3];
+    unsigned char codigo_F_F_I[500];
+    func_ptr_0_P teste_F_F_I;
+
+    params_F_F_I[0].tipo_val = INT_PAR;
+    params_F_F_I[0].orig_val = FIX;
+    params_F_F_I[0].valor.v_int = 15;
+
+    params_F_F_I[1].tipo_val = INT_PAR;
+    params_F_F_I[1].orig_val = FIX;
+    params_F_F_I[1].valor.v_int = 3;
+
+    params_F_F_I[2].tipo_val = INT_PAR; 
+    params_F_F_I[2].orig_val = PARAM;
+
+    cria_func(testa_3I, params_F_F_I, 3, codigo_F_F_I);
+    teste_F_F_I = (func_ptr_0_P) codigo_F_F_I;
+    int resultado_F_F_I = teste_F_F_I(2);
+    printf("\nFIX - FIX - PARAM : ");
+    
+    if( resultado_F_F_I == 90)
+    {
+        printf("PASSOU NO TESTE\n");
+    }
+    else{
+        printf("NÃO PASSOU NO TESTE\n");
+
+    } 
 
        
 }
